20ef8ab226afb1a68bd8b9c8f9f416600ac28565
Michał Niewitała <niewitala@lightweb.pl>
2025-04-28 12:09:19 +0200

    Update jokes

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,14 +1,6 @@
 import jokes from './jokes.json';
 
 export const handler = async () => {
-  const sth = JSON.parse(
-    Buffer.from(
-      process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS,
-      'base64',
-    ).toString('utf8'),
-  );
-  console.log(sth);
-
   console.log(process.env.LARGE);
 
   const randomIndex = Math.floor(Math.random() * jokes.length);
